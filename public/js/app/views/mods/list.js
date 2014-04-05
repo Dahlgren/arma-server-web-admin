8ef61ed197d2c2e84f9f9ff71965a7511e858c6d
Björn Dahlgren <bjorn@dahlgren.at>
2014-04-05 19:36:34 +0000

    Use table for mods list

--- public/js/app/views/mods/list.js
+++ public/js/app/views/mods/list.js
@@ -1,15 +1,19 @@
 define(function (require) {
-  
+
   "use strict";
-  
+
   var $                   = require('jquery'),
       _                   = require('underscore'),
       Backbone            = require('backbone'),
       Marionette          = require('marionette'),
-      ListItemView        = require('app/views/mods/list_item');
-  
-  return Marionette.CollectionView.extend({
-    tagName: 'ul',
+      ListItemView        = require('app/views/mods/list_item'),
+      tpl                 = require('text!tpl/mods/list.html'),
+
+      template = _.template(tpl);
+
+  return Marionette.CompositeView.extend({
     itemView: ListItemView,
+    itemViewContainer: "tbody",
+    template: template,
   });
 });
