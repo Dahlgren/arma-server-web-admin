995a43e7e4fc0c989efa5e87077b5f6da0763a6e
Björn Dahlgren <bjorn@dahlgren.at>
2014-04-07 00:28:23 +0200

    Fixed tabs behaviour

--- public/js/app/views/servers/view.js
+++ public/js/app/views/servers/view.js
@@ -1,7 +1,7 @@
 define(function (require) {
-  
+
   "use strict";
-  
+
   var $                   = require('jquery'),
       _                   = require('underscore'),
       Backbone            = require('backbone'),
@@ -9,21 +9,30 @@ define(function (require) {
       Mods                = require('app/collections/mods'),
       InfoView            = require('app/views/servers/info'),
       ModsListView        = require('app/views/mods/list'),
-      tpl                 = require('text!tpl/servers/view.html');    
-  
+      tpl                 = require('text!tpl/servers/view.html');
+
   return Marionette.Layout.extend({
     template: _.template(tpl),
-    
+
     regions: {
-      info: "#info",
-      mods: "#mods",
-      settings: "#settings"
+      info: "#tab-info",
+      mods: "#tab-mods",
+      settings: "#tab-settings"
     },
-    
+
+    events: {
+      "click .nav-tabs a" : "tabs",
+    },
+
     onRender: function() {
       this.info.show(new InfoView({model: this.model}));
       this.mods.show(new ModsListView({collection: new Mods(this.model.get('mods'))}));
     },
+
+    tabs: function(e) {
+      e.preventDefault()
+      $($(e.target).attr('href')).tab('show')
+    },
   });
-  
+
 });
