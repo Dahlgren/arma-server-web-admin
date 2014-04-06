269cb62ec15e9e22c1c74c2bb9d580cc4c47c204
Björn Dahlgren <bjorn@dahlgren.at>
2014-04-07 01:01:34 +0200

    Do not reload until upload is complete

--- public/js/app/views/missions/form.js
+++ public/js/app/views/missions/form.js
@@ -1,7 +1,7 @@
 define(function (require) {
-  
+
   "use strict";
-  
+
   var $                   = require('jquery'),
       _                   = require('underscore'),
       Backbone            = require('backbone'),
@@ -9,32 +9,32 @@ define(function (require) {
       FormView            = require('marionette-formview'),
       IframeTransport     = require('jquery.iframe-transport'),
       Mission             = require('app/models/mission'),
-      tpl                 = require('text!tpl/missions/form.html');    
-  
+      tpl                 = require('text!tpl/missions/form.html');
+
   return Marionette.ItemView.extend({
     template: _.template(tpl),
-    
+
     initialize: function (options) {
       this.missions = options.missions;
       this.model = new Mission();
       this.bind("ok", this.submit);
     },
-    
+
     submit: function (modal) {
       var self = this;
-      
+
       modal.preventClose();
-      
+
       var $form = $("form");
       $.ajax("/api/missions", {
         files: $form.find(":file"),
         iframe: true
       }).complete(function(data) {
         modal.close();
-        self.missions.fetch();
-        Backbone.history.navigate('#missions', true)
+        self.missions.fetch({success : function () {
+          Backbone.history.navigate('#missions', true);
+        }});
       });
     },
   });
-  
 });
