7d11af4633078bfb3645c0e7d3b44aec4ddff854
Björn Dahlgren <bjorn@dahlgren.at>
2014-04-06 01:10:09 +0000

    Return folders and files when fetching a mod

--- routes/mods.js
+++ routes/mods.js
@@ -1,6 +1,46 @@
-var fs = require('fs');
+var fs = require('fs')
+, path = require('path')
+, when = require('when')
+, nodefn = require('when/node/function');
 
 var config = require('./../config');
+
+function walk (directory) {
+  createFile = function (file, stat) {
+    return {
+      type: "file",
+      name: file,
+      size: stat.size
+    };
+  };
+
+  createFolder = function (folder) {
+    return {
+      type: "folder",
+      name: folder,
+      files: []
+    };
+  };
+
+  var results = [];
+
+  return when.map(nodefn.call(fs.readdir, directory), function(file) {
+    var absolutePath = path.join(directory, file);
+    return nodefn.call(fs.stat, absolutePath).then(function(stat) {
+      if (stat.isFile()) {
+        return results.push(createFile(file, stat));
+      }
+
+      folder = createFolder(file);
+      return walk(absolutePath).then(function(filesInDir) {
+        folder.files = filesInDir;
+        results.push(folder);
+      });
+    });
+  }).then(function() {
+    return results;
+  });
+};
 
 exports.index = function(req, res){
   fs.readdir(config.path, function (err, files) {
@@ -22,7 +62,12 @@ exports.create = function(req, res){
 };
 
 exports.show = function(req, res){
-  res.send('show mod ' + req.params.mod);
+  walk(config.path + "/" + req.params.mod).then(function(files) {
+    res.json(files);
+  }).otherwise(function(error) {
+    console.error(error.stack || error);
+    res.send({success: false});
+  });
 };
 
 exports.destroy = function(req, res){
