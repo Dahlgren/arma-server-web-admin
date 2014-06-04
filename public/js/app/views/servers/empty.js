957e665ff2b6d6f7a1477d6dfe3c397bd79fcf69
Björn Dahlgren <bjorn@dahlgren.at>
2014-06-05 00:29:31 +0200

    Button to add more servers

--- public/js/app/views/servers/empty.js
+++ public/js/app/views/servers/empty.js
@@ -6,23 +6,13 @@ define(function (require) {
       _                   = require('underscore'),
       Backbone            = require('backbone'),
       Marionette          = require('marionette'),
-      AddServerView       = require('app/views/servers/add'),
       tpl                 = require('text!tpl/servers/empty.html');
 
   return Marionette.ItemView.extend({
     template: _.template(tpl),
 
-    events: {
-      "click #add-server": "addServer"
-    },
-
     initialize: function (options) {
       this.servers = options.servers;
-    },
-
-    addServer: function () {
-      var view = new AddServerView({servers: this.servers});
-      new Backbone.BootstrapModal({ content: view, servers: this.servers }).open()
     },
   });
 
--- public/js/app/views/servers/list.js
+++ public/js/app/views/servers/list.js
@@ -6,6 +6,7 @@ define(function (require) {
       _                   = require('underscore'),
       Backbone            = require('backbone'),
       Marionette          = require('marionette'),
+      AddServerView       = require('app/views/servers/add'),
       EmptyView           = require('app/views/servers/empty'),
       ListItemView        = require('app/views/servers/list_item'),
       tpl                 = require('text!tpl/servers/list.html'),
@@ -19,6 +20,10 @@ define(function (require) {
 
     emptyView: EmptyView,
 
+    events: {
+      "click #add-server": "addServer"
+    },
+
     buildItemView: function(item, ItemViewType, itemViewOptions){
       // build the final list of options for the item view type
       var options = _.extend({model: item}, itemViewOptions);
@@ -31,6 +36,11 @@ define(function (require) {
       var view = new ItemViewType(options);
       // return it
       return view;
+    },
+
+    addServer: function () {
+      var view = new AddServerView({servers: this.collection});
+      new Backbone.BootstrapModal({ content: view, servers: this.collection }).open()
     },
   });
 });
