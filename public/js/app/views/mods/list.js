a39e6f17e05e083fe325381e802c59ad6353bed7
Björn Dahlgren <bjorn@dahlgren.at>
2017-02-21 20:22:28 +0100

    Remove Play withSIX

--- public/js/app/views/mods/list.js
+++ public/js/app/views/mods/list.js
@@ -7,7 +7,6 @@ define(function (require) {
       Backbone            = require('backbone'),
       Marionette          = require('marionette'),
       ListItemView        = require('app/views/mods/list_item'),
-      FormView            = require('app/views/mods/form'),
       tpl                 = require('text!tpl/mods/list.html'),
 
       template = _.template(tpl);
@@ -18,25 +17,11 @@ define(function (require) {
     template: template,
 
     events: {
-      "click #download": "download",
       "click #refresh": "refresh",
     },
 
     initialize: function (options) {
       this.listenTo(this.collection, "change reset", this.render);
-    },
-
-    download: function (event) {
-      event.preventDefault();
-      var view = new FormView({mods: this.collection});
-      var modal = new Backbone.BootstrapModal({
-        content: view,
-        animate: true,
-        cancelText: 'Close',
-        okText: 'Search',
-      });
-      view.modal = modal;
-      modal.open();
     },
 
     refresh: function (event) {
