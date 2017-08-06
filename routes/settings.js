b37342e1008d7e23d19813df81302f23fa27e828
Björn Dahlgren <bjorn@dahlgren.at>
2017-08-06 11:30:04 +0200

    Whitelist which config settings to send for API endpoint
    Avoids sharing private username and password for Steam etc

--- routes/settings.js
+++ routes/settings.js
@@ -1,10 +1,11 @@
 var express = require('express')
+var _ = require('lodash')
 
 module.exports = function (config) {
   var router = express.Router()
 
   router.get('/', function (req, res) {
-    res.json(config)
+    res.json(_.pick(config, ['game', 'path', 'type']))
   })
 
   return router
