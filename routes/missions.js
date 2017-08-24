853a79546ab81393330e1170d3ff27643e3e775f
Björn Dahlgren <bjorn@dahlgren.at>
2017-08-24 11:15:06 +0200

    Lint and format all backend code with standard.js

--- routes/missions.js
+++ routes/missions.js
@@ -1,65 +1,65 @@
-var express = require('express');
-var multer  = require('multer');
-var upload = multer({ storage: multer.diskStorage({}) });
+var express = require('express')
+var multer = require('multer')
+var upload = multer({ storage: multer.diskStorage({}) })
 
 module.exports = function (missionsManager) {
-  var router = express.Router();
+  var router = express.Router()
 
   router.get('/', function (req, res) {
     missionsManager.list(function (err, missions) {
       if (err) {
-        res.status(500).send(err);
+        res.status(500).send(err)
       } else {
-        res.json(missions);
+        res.json(missions)
       }
-    });
-  });
+    })
+  })
 
   router.post('/', upload.single('mission'), function (req, res) {
-    var missionFile = req.file;
+    var missionFile = req.file
 
     if (!missionFile) {
-      return res.status(400).send('No mission file uploaded');;
+      return res.status(400).send('No mission file uploaded')
     }
 
     missionsManager.handleUpload(missionFile, function (err) {
       if (err) {
-        res.status(500).send(err);
+        res.status(500).send(err)
       } else {
-        res.status(200).json({success: true});
+        res.status(200).json({success: true})
       }
-    });
-  });
+    })
+  })
 
   router.get('/:mission', function (req, res) {
-    var filename = req.params.mission;
+    var filename = req.params.mission
 
-    res.download(missionsManager.missionPath(filename), decodeURI(filename));
-  });
+    res.download(missionsManager.missionPath(filename), decodeURI(filename))
+  })
 
   router.delete('/:mission', function (req, res) {
-    var filename = req.params.mission;
+    var filename = req.params.mission
 
     missionsManager.delete(filename, function (err) {
       if (err) {
-        res.status(500).send(err);
+        res.status(500).send(err)
       } else {
-        res.json({success: true});
+        res.json({success: true})
       }
-    });
-  });
+    })
+  })
 
   router.post('/workshop', function (req, res) {
-    var id = req.body.id;
+    var id = req.body.id
 
     missionsManager.downloadSteamWorkshop(id, function (err, files) {
       if (err) {
-        res.status(500).send(err);
+        res.status(500).send(err)
       } else {
-        res.json({success: true});
+        res.json({success: true})
       }
-    });
-  });
+    })
+  })
 
-  return router;
-};
+  return router
+}
--- routes/servers.js
+++ routes/servers.js
@@ -1,50 +1,50 @@
-var express = require('express');
+var express = require('express')
 
 module.exports = function (manager, mods) {
-  var router = express.Router();
+  var router = express.Router()
 
   router.get('/', function (req, res) {
-    res.json(manager.getServers());
-  });
+    res.json(manager.getServers())
+  })
 
   router.post('/', function (req, res) {
-    var server = manager.addServer(req.body);
-    res.json(server);
-  });
+    var server = manager.addServer(req.body)
+    res.json(server)
+  })
 
   router.get('/:server', function (req, res) {
-    var server = manager.getServer(req.params.server);
-    res.json(server);
-  });
+    var server = manager.getServer(req.params.server)
+    res.json(server)
+  })
 
   router.put('/:server', function (req, res) {
-    var server = manager.getServer(req.params.server);
-    server.update(req.body);
-    manager.save();
-    res.json(server);
-  });
+    var server = manager.getServer(req.params.server)
+    server.update(req.body)
+    manager.save()
+    res.json(server)
+  })
 
   router.delete('/:server', function (req, res) {
-    var server = manager.removeServer(req.params.server);
-    res.json(server);
-  });
+    var server = manager.removeServer(req.params.server)
+    res.json(server)
+  })
 
   router.post('/:server/start', function (req, res) {
-    var server = manager.getServer(req.params.server);
-    server.start();
-    res.json({status:"ok", pid: server.pid});
-  });
+    var server = manager.getServer(req.params.server)
+    server.start()
+    res.json({status: 'ok', pid: server.pid})
+  })
 
   router.post('/:server/stop', function (req, res) {
-    var server = manager.getServer(req.params.server);
+    var server = manager.getServer(req.params.server)
     server.stop(function () {
       if (!server.pid) {
-        res.json({status: true, pid: server.pid});
+        res.json({status: true, pid: server.pid})
       } else {
-        res.json({status: false, pid: server.pid});
+        res.json({status: false, pid: server.pid})
       }
-    });
-  });
+    })
+  })
 
-  return router;
-};
+  return router
+}
