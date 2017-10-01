6c979baeecc29931b24498f8cb373e25dc091d5a
Moritz Schmidt <fusselwurm@gmail.com>
2017-10-01 17:46:43 +0200

    enable user to view log file in browser

--- routes/logs.js
+++ routes/logs.js
@@ -13,15 +13,26 @@ module.exports = function (logsManager) {
     })
   })
 
-  router.get('/:log', function (req, res) {
+  router.get('/:log/:mode', function (req, res) {
     var requestedFilename = req.params.log
+    var mode = req.params.mode === 'view' ? 'view' : 'download'
 
     logsManager.getLogFile(requestedFilename, function (err, file) {
       if (err) {
         res.status(500).send(err)
       } else {
         if (file) {
-          res.download(file.path)
+          if (mode === 'download') {
+            res.download(file.path)
+          } else {
+            logsManager.readLogFile(file.path, function (err, data) {
+              if (err) {
+                return res.status(500).send(err)
+              }
+              res.contentType('text/plain')
+              res.send(data)
+            })
+          }
         } else {
           res.status(404).send(new Error('File not found'))
         }
