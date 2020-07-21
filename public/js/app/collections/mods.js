6bef8bfe85cb1238713bcfa90b26cbb1e673d033
Najsr <dajces94@gmail.com>
2020-07-21 11:36:37 +0200

    Made it same as servers

--- public/js/app/collections/mods.js
+++ public/js/app/collections/mods.js
@@ -3,7 +3,9 @@ var Backbone = require('backbone')
 var Mod = require('app/models/mod')
 
 module.exports = Backbone.Collection.extend({
-  comparator: function(item) { item.get('name').toLowerCase() },
+  comparator: function (a, b) {
+    return a.get('name').toLowerCase().localeCompare(b.get('name').toLowerCase())
+  },
   model: Mod,
   url: '/api/mods/'
 })
