0d9b6134b7392a279277a8bf7adbe7df815132cc
Björn Dahlgren <bjorn@dahlgren.at>
2021-02-28 14:30:16 +0100

    Sort logs by creation date in descending order

--- public/js/app/collections/logs.js
+++ public/js/app/collections/logs.js
@@ -3,7 +3,9 @@ var Backbone = require('backbone')
 var Log = require('app/models/log')
 
 module.exports = Backbone.Collection.extend({
-  comparator: 'name',
+  comparator: function (a, b) {
+    return b.get('created').localeCompare(a.get('created')) // Descending order
+  },
   model: Log,
   url: '/api/logs/'
 })
