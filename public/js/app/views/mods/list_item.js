955b8215a4d2814bd30004bbb5232c824ac01940
Björn Dahlgren <bjorn@dahlgren.io>
2021-07-10 15:54:31 +0200

    Get mod status from Steam Workshop

--- public/js/app/views/mods/list_item.js
+++ public/js/app/views/mods/list_item.js
@@ -1,4 +1,6 @@
+var $ = require('jquery')
 var _ = require('underscore')
+var Ladda = require('ladda')
 var Marionette = require('marionette')
 var sweetAlert = require('sweet-alert')
 
@@ -11,11 +13,38 @@ module.exports = Marionette.ItemView.extend({
   template: template,
 
   events: {
+    'click .install': 'installMod',
     'click .destroy': 'deleteMod'
   },
 
   modelEvents: {
     change: 'render'
+  },
+
+  installMod: function (event) {
+    var self = this
+    event.preventDefault()
+
+    this.laddaBtn = Ladda.create(this.$el.find('.ladda-button').get(0))
+    this.laddaBtn.start()
+    this.$el.find('.ladda-button').addClass('disabled')
+
+    $.ajax({
+      url: '/api/mods/',
+      type: 'POST',
+      data: {
+        id: this.model.get('id')
+      },
+      dataType: 'json',
+      success: function (resp) {
+        self.laddaBtn.stop()
+        self.$el.find('.ladda-button').removeClass('disabled')
+      },
+      error: function (resp) {
+        self.laddaBtn.stop()
+        self.$el.find('.ladda-button').removeClass('disabled')
+      }
+    })
   },
 
   deleteMod: function (event) {
